5f21f148e8deb5d41ee67814104f580cbca93332
mathiasburton <mathias.burton@tylertech.com>
2018-11-09 16:38:02 -0800

    deployment check?

---
+++ codeship-practice/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mathias.burton/Dropbox/Studies/_Active Studies/Development/_Projects/005_Docker/Django-to-do/python-django-todoapp/codeship-practice/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
